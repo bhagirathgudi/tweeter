766e40c72e9cf623db2d0b7140e3c57d90d527d3
bhagirathgudi <bhagirath009@hotmail.com>
2018-11-02 14:11:38 -0500

    Populating properties from environment variables.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var Twitter = require('twitter');
 
 var client = new Twitter({
-    consumer_key : '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
+    consumer_key : process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-var params = {screen_name: ''};
+var params = {screen_name: process.env.SCREEN_NAME};
 // client.get('statuses/user_timeline', params, function(error, tweets, response) {
 //     if(!error) {
 //         console.log(tweets);
