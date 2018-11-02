acfb6fc4d81f6353ef75ac27b23ff7745894f082
bhagirathgudi <bhagirath009@hotmail.com>
2018-11-02 14:02:33 -0500

    removing hardcoded values.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var Twitter = require('twitter');
 
 var client = new Twitter({
-    consumer_key : '6G61eCnaKSOVlT2vPwyA45lOb',
-    consumer_secret: 'eXdKNpxLwssp3ABRclIA37samLwSW17BYVMZunLNRRmfPE607B',
-    access_token_key: '2506190910-a3R17bBA5ineN3l11ks65nj40Jj33swARw77Y7B',
-    access_token_secret: 'DpFFHoaYmhzUkwjSilDopjTWx4p6v8Nt8mrsvS4080R2R'
+    consumer_key : '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
 });
 
-var params = {screen_name: 'bhagi_009'};
+var params = {screen_name: ''};
 // client.get('statuses/user_timeline', params, function(error, tweets, response) {
 //     if(!error) {
 //         console.log(tweets);
